148a72a2171f358bd84d378612f2e79246c36443
tami <tamra-oneil@pluralsight.com>
2018-05-31 11:13:26 -0600

    finished greeter kata, setup prime factors kata files

--- src/Greeter-tk/greeter.js
+++ src/Greeter-tk/greeter.js
@@ -2,15 +2,25 @@ function Greeter(name, date = new Date()) {
   let displayName = name.trim();
   displayName = displayName.replace(displayName[0], displayName[0].toUpperCase());
 
-  return isMorning(date) ? `Good morning ${displayName}` : `Hello ${displayName}`;
+  const greeting = getGreeting(date, displayName);
+
+  console.log(greeting);
+
+  return greeting;
 }
 
-function isMorning(date) {
+function getGreeting(date, name) {
   const d = new Date(date);
+
   if (d.getHours() >= 6 && d.getHours() <= 12) {
-    return true;
+    return `Good morning ${name}`;
+  } else if (d.getHours() >= 18 && d.getHours() <= 22) {
+    return `Good evening ${name}`;
+  } else if (d.getHours() > 22 || d.getHours() < 6) {
+    return `Good night ${name}`;
   }
-  return false;
+
+  return `Hello ${name}`;
 }
 
 module.exports = Greeter;
--- src/Greeter-tk/greeter.test.js
+++ src/Greeter-tk/greeter.test.js
@@ -2,19 +2,42 @@ const greeter = require('./greeter');
 
 describe('#greeter', () => {
   test('should return Hello Kitty when passed Kitty', () => {
-    expect(greeter('Kitty')).toBe('Hello Kitty');
+    const date = '2018-05-15T19:21:47.992Z';
+    expect(greeter('Kitty', date)).toBe('Hello Kitty');
   });
   test('should return Hello Puppy when passed Puppy', () => {
-    expect(greeter('Puppy')).toBe('Hello Puppy');
+    const date = '2018-05-15T19:21:47.992Z';
+    expect(greeter('Puppy', date)).toBe('Hello Puppy');
   });
   test('should return Hello Kelly when passed Kelly with extra spaces', () => {
-    expect(greeter(' Kelly ')).toBe('Hello Kelly');
+    const date = '2018-05-15T19:21:47.992Z';
+    expect(greeter(' Kelly ', date)).toBe('Hello Kelly');
   });
   test('should return Hello Tami when passed tami with lower case t', () => {
-    expect(greeter('tami')).toBe('Hello Tami');
+    const date = '2018-05-15T19:21:47.992Z';
+    expect(greeter('tami', date)).toBe('Hello Tami');
   });
   test('should return Good morning Kush when the time is between 6am-12pm', () => {
     const date = '2018-05-15T12:21:47.992Z';
     expect(greeter('Kush', date)).toBe('Good morning Kush');
+  });
+  test('should return Good evening Tanner when the time is between 18-22pm', () => {
+    const date = '2018-05-15T00:21:47.992Z';
+    expect(greeter('Tanner', date)).toBe('Good evening Tanner');
+  });
+  test('should return Good Night Michael when the time is between 23pm-06am', () => {
+    const date = '2018-05-15T11:21:47.992Z';
+    expect(greeter('Michael', date)).toBe('Good night Michael');
+  });
+  test('should call console.log whenever function is called', () => {
+    const date = '2018-05-15T19:21:47.992Z';
+    const spy = jest.spyOn(console, 'log');
+
+    greeter('Party People', date);
+
+    expect(spy).toHaveBeenCalled();
+
+    spy.mockReset();
+    spy.mockRestore();
   });
 });
